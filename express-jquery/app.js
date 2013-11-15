a948a6aa30bf1470d1817ca8150f02681e210662
schiluka <san.chiluka@gmail.com>
2013-11-15 15:48:38 -0500

    async added

--- express-jquery/app.js
+++ express-jquery/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var async = require('async');
 //var request = require('request');
 
 var app = express();
@@ -33,6 +34,28 @@ app.get('/submitForm', function(req, res) {
 	res.send('//www.youtube.com/embed/JhtdC86wWIk');
 });
 
+app.get('/async', function(req, res, next) {
+    var userId1;
+	var userId2;
+	console.log('async started');
+    async.series([
+        //1st db call or web service call
+        function(callback) {
+			userId1 = 'result1 from first function'; //Set the userId here, so the next task can access it
+			callback(null, 'one');
+        },
+        //second db call or web service call
+        function(callback) {
+            userId2 = userId1 + '<br>result2 from second function';
+			callback(null, 'two');
+        }
+    ], function(err, results) { //This function gets called after the two tasks have called their "task callbacks"
+        if (err) return next(err);
+        //Here locals will be populated with 'user' and 'posts'
+		console.log(results);
+        res.send('<h1>'+userId2+'</h1>');
+    });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
